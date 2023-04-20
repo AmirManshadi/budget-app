f14f1e04fe91f8ddef75f6d3b02bb9f58455a8b3
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-04-20 20:31:24 +0430

    added routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+// rrd imports
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from "react-router-dom"
+
+// layout
+import RootLayout from "./layouts/RootLayout"
+
+// pages
+import Dashboard from "./pages/Dashboard"
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />} errorElement={<h1>error</h1>}>
+			<Route index element={<Dashboard />}></Route>
+		</Route>
+	)
+)
+
 function App() {
-	return <>hello world</>
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	)
 }
 
 export default App
