0c2be2712d842914e994b1b3272d87b7a19755e8
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-04-20 20:30:25 +0430

    created RootLayout.jsx

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+function RootLayout() {
+	return (
+		<>
+			<div>RootLayout</div>
+			<main>
+				<Outlet />
+			</main>
+		</>
+	)
+}
+export default RootLayout
