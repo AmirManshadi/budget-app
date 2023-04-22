2fe080a8e8f647de91bd98fb444db2719e1caf47
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-04-22 21:08:20 +0430

    added NavbarLogoutAction to logout the user (remove the userName from localStorage)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,36 @@
 // rrd imports
-import { Form, NavLink } from "react-router-dom"
+import { Form, NavLink, redirect } from "react-router-dom"
 
 // libraries
 import { HomeIcon } from "@heroicons/react/24/outline"
 
-function Navbar() {
+// Components
+import Button from "./Button"
+
+// Helpers
+import { deleteData } from "../utils/helperFunctions"
+
+// Action
+export async function NavbarLogoutAction() {
+	deleteData("userName")
+	return redirect("/")
+}
+
+// eslint-disable-next-line react/prop-types
+function Navbar({ userName }) {
 	return (
 		<nav>
-			<NavLink to="/" className={"website-title"}>
-				<HomeIcon width={40} />
-				<h1>BudgetApp</h1>
+			<NavLink to="/" className="website-title">
+				<h1>
+					<HomeIcon width={40} />
+					BudgetApp
+				</h1>
 			</NavLink>
-			<Form action="/logout"></Form>
+			{userName && (
+				<Form method="delete" action="/logout">
+					<Button style="danger">Logout</Button>
+				</Form>
+			)}
 		</nav>
 	)
 }
