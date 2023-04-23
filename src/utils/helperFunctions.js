7efc19276a41c8d044585fea6575137d0626db72
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-04-23 11:09:46 +0430

    feat(deleteData): create toast for deleting data

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,9 +1,23 @@
+// Libraries
+import { toast } from "react-toastify"
+
 export function fetchData(key) {
 	const result = localStorage.getItem(key)
 	return result
 }
 
 export function deleteData(key) {
-	const result = localStorage.removeItem(key)
-	return result
+	if (confirm("Do you want to Log out?")) {
+		localStorage.removeItem(key)
+	}
+	return toast.success("You have successfully logged out.", {
+		position: "bottom-left",
+		autoClose: 3000,
+		hideProgressBar: false,
+		closeOnClick: true,
+		pauseOnHover: true,
+		draggable: true,
+		progress: undefined,
+		theme: "light",
+	})
 }
