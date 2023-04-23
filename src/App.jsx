cf02f397bc94cd2adae6819748e168a5ee113621
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-04-23 10:33:40 +0430

    feat(app): add react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import {
 	RouterProvider,
 } from "react-router-dom"
 
+// Libraries
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.min.css"
+
 // Layout
 import RootLayout, { RootLayoutLoader } from "./layouts/RootLayout"
 
 // Pages
 import Dashboard from "./pages/Dashboard"
+import Error404 from "./pages/Error404"
 
 // Actions
 import { NavbarLogoutAction } from "./components/Navbar"
-import Error404 from "./pages/Error404"
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<RootLayout />} loader={RootLayoutLoader}>
 			<Route index element={<Dashboard />}></Route>
 			<Route path="logout" action={NavbarLogoutAction}></Route>
-      <Route path="*" element={<Error404 />}/>
+			<Route path="*" element={<Error404 />} />
 		</Route>
 	)
 )
@@ -30,6 +34,18 @@ function App() {
 	return (
 		<>
 			<RouterProvider router={router} />
+			<ToastContainer
+				position="bottom-left"
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
 		</>
 	)
 }
