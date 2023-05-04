ff44c4bd5d897cdc387830a69f19e026b2af1a78
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-05-04 18:38:47 +0430

    style: add style to error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -8,20 +8,24 @@ function Error() {
 	const error = useRouteError()
 	const navigate = useNavigate()
 	return (
-		<>
+		<div className="error">
 			<h1>Uh Oh! something went wrong</h1>
 			<p>{error.message || error.statusText}</p>
 			<div>
-				<button onClick={() => navigate(-1)}>
+				<button
+					type="button"
+					onClick={() => navigate(-1)}
+					className="home-button"
+				>
 					<span>Go Back</span>
 					<ArrowUturnLeftIcon width={20} />
 				</button>
-				<Link to="/">
+				<Link to="/" className="back-button">
 					<span>Home Page</span>
 					<HomeIcon width={20} />
 				</Link>
 			</div>
-		</>
+		</div>
 	)
 }
 export default Error
