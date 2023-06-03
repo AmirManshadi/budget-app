4857e83a331ee20dd2734799836525584d78b2ed
Amirhossein Pourimanshad <99075685+AmirManshadi@users.noreply.github.com>
2023-06-03 18:02:52 +0330

    feat: create expense page

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,25 @@
+// rrd imports
+import { useLoaderData } from "react-router-dom"
+
+// helper functions
+import { fetchData } from "../utils/helperFunctions"
+
+// component imports
+import Table from "./Table"
+
+export function ExpensesLoader() {
+	const expenses = fetchData("expenses")
+	return { expenses }
+}
+
+export default function Expenses() {
+	const expenses = useLoaderData()
+	return (
+		<div>
+			<h2>
+				Your Expenses <small>({expenses.length} total)</small>
+			</h2>
+			<Table expenses={expenses} />
+		</div>
+	)
+}
